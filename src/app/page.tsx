259e6f66c508c1faec70fed2d42576d43378f9c3
ratiborU <tvinck_444@mail.ru>
2025-04-05 01:46:40 +0500

    fix2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import ClientHeader from "@/components/ClientHeader/ClientHeader";
 import styles from "./page.module.css";
 // import SliderComponent from "@/components/Slider/Slider";
-import { getAllProducts } from "@/services/api/products/productService";
+// import { getAllProducts } from "@/services/api/products/productService";
 import HomeComments from "@/widjets/HomeComments/HomeComments";
 import PopularProducts from "@/widjets/PopularProducts/PopularProducts";
 import SpecialOfferProducts from "@/widjets/SpecialOfferProducts/SpecialOfferProducts";
